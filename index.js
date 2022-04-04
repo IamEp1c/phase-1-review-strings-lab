8263fb1f36267fbf56b751910d29b3267115aee6
Waqas Anjum <Qanjum96@gmail.com>
2022-04-04 12:40:08 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+let currentUser = 'Qaz'
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+let excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase(); 
+
+let shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
